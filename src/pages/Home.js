6b2953503ff8db146afa32612899f125f51de0f0
ZamIzzudin <azzamizzudinhasan@gmail.com>
2024-01-03 00:06:46 +0700

    style:(enhance responsive and layouting home page)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,27 @@
+import bg from '../assets/hand_bf2.png'
+import bg2 from '../assets/hand_bf2(1).png'
+
+import style from '../styles/pages/Home.module.css'
+
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome, Odin</h1>
-    </div>
+    <section className={style.page_layout}>
+      <div className={style.page_content}>
+        <span>Hi, Odin</span>
+        <h1>WELCOME TO <br /> TEDX DASHBOARD</h1>
+        <img className={style.bg1} src={bg} alt="hand background" width="50%" />
+        <img className={style.bg2} src={bg2} alt="hand background" width="50%" />
+      </div>
+      {/* <div className={style.modern_carousel}>
+        <div className={style.box_wrapper}>
+          <div className={style.side}></div>
+          <div className={style.side}></div>
+          <div className={style.side}></div>
+          <div className={style.side}></div>
+          <div className={style.side}></div>
+          <div className={style.side}></div>
+        </div>
+      </div> */}
+    </section>
   );
 }
